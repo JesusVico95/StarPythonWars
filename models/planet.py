d7fe70a7c272a475788369e9f08f567fe6f4d4b1
unknown <jesusvico203@gmail.com>
2025-09-13 11:50:26 +0200

    feat(models): Obtain planets with films and filter diameter

---
+++ models/planet.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+from typing import List
+from models.film import Film
+from client.resource import ApiResource
+from models.errors import ErrorParameterNotValid
+import logging
+class Planet(ApiResource):
+    def __init__(self, name:str = None, diameter:str = None,
+                 list_of_films:List=None):
+        self._name = name
+        self._diameter = diameter
+        self._list_of_films = list_of_films
+
+    @staticmethod
+    def parse_planets(response_about_planet:List[dict]):
+        list_planets = []
+
+        for planet in response_about_planet:
+            planet = Planet.from_api_response(planet)
+            list_planets.append(planet)
+
+        return list_planets
+
+    @classmethod
+    def from_api_response(cls, planet:dict):
+        from client.client import StarWarsCallApi
+        film_names = StarWarsCallApi.fetch_by_url(planet["films"],Film)
+
+        planet = Planet(planet["name"],planet["diameter"],film_names)
+        return planet
+
+    @staticmethod
+    def filter_by_diameter(list_planets:List[Planet]):
+        planets_without_diameter_unknown:List[Planet] = []
+
+        try:
+            diameter = Planet.obtain_a_diameter()
+        except ErrorParameterNotValid as e:
+            logging.error(f"{e}")
+            return []
+
+        for planets in list_planets:
+            if planets._diameter == "unknown":
+                continue
+            planets_without_diameter_unknown.append(planets)
+
+        query = list(filter(lambda d: int(d._diameter) > diameter,
+                            planets_without_diameter_unknown))
+
+        return query
+
+    @staticmethod
+    def obtain_a_diameter():
+            valid_number = True
+            while valid_number:
+                try:
+                    diameter = int(input(f"Introduce a number between "
+                    f"10.000 and 14.000: "))
+                    if diameter >=10000 and diameter<= 14000:
+                        valid_number = False
+                        return diameter
+                except ValueError as e:
+                    raise ErrorParameterNotValid(f"The paramater 'diameter'"
+                                                 f"must be integer")
+
+    @property
+    def name(self):
+        return self._name
+    @property
+    def diameter(self):
+        return self._diameter
+
+    def __str__(self):
+        films_text = ""
+        if self._list_of_films:
+            for film in self._list_of_films:
+                films_text += "\n"+str(film) + " "
+
+        return f"Name : {self._name}, Diameter : {self._diameter} {films_text}"
