3160685fa8fc7ee30df232694d7e5610a6b3c770
unknown <jesusvico203@gmail.com>
2025-09-13 11:37:36 +0200

    feat(models): Add custom exceptions

---
+++ models/errors.py
@@ -0,0 +1,14 @@
+class ErrorUrlInvalid(BaseException):
+    pass
+
+class ErrorParameterNotValid(BaseException):
+    pass
+
+class ErrorNegativeNumber(BaseException):
+    pass
+
+class ErrorExceptTest(BaseException):
+    pass
+
+class ErrorToParse(BaseException):
+    pass
