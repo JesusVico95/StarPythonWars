d3ae9c678b9d4d235d50a4f06a6942788a25ff0c
unknown <jesusvico203@gmail.com>
2025-09-15 20:52:58 +0200

    feat(models/people): Refactor code and check validate inputs

--- models/people.py
+++ models/people.py
@@ -4,6 +4,8 @@ from models.film import Film
 from models.starship import Starship
 from models.vehicle import Vehicle
 from client.resource import ApiResource
+import re
+from models.errors import ErrorParameterNotValid
 
 class People(ApiResource):
     def __init__(self, name:str, homeworld:List[str], films:List[Film],
@@ -14,7 +16,7 @@ class People(ApiResource):
         self._starships = starships
         self._vehicles = vehicles
     @classmethod
-    def from_api_response(cls, data:dict):
+    def from_api_response(cls, data:dict) -> People:
         from client.client import StarWarsCallApi
         starships = StarWarsCallApi.fetch_by_url(tuple(data["starships"]),Starship)
         films = StarWarsCallApi.fetch_by_url(tuple(data["films"]),Film)
@@ -24,30 +26,52 @@ class People(ApiResource):
         return cls(name=data["name"],homeworld=planet,films=films,
                    starships=starships,vehicles=vehicles)
 
-    @staticmethod
-    def filter_by_character(character_list:List[People], name_character:str)\
+    @classmethod
+    def filter_by_character(cls,character_list:List[People], name_character:str)\
     -> People:
+        clean_name_character = cls.validate_input(name_character)
         for character in character_list:
-            if character._name.lower() == name_character:
+            if character._name.lower() == clean_name_character.lower():
                 return character
 
-    def from_api(self) -> List[People]:
-        from client.client import StarWarsCallApi
-        sw_api = StarWarsCallApi()
-        data_response = sw_api.get_all_information\
-        (sw_api.api_endpoints["people"])
-        return [self.from_api_response(instance) for instance in data_response]
+        return None
 
-    def get_character_names(self) -> List[str]:
+    @classmethod
+    def validate_input(cls,name_character:str) -> str:
+        clean_name = name_character.strip()
+
+        if not clean_name:
+            raise ErrorParameterNotValid("The name can't be empty.")
+
+        if not re.fullmatch(r"[A-Za-z0-9 \-]+", clean_name):
+            raise ErrorParameterNotValid(
+                f"Invalid name. Input: '{name_character}'"
+            )
+
+        if clean_name.isdigit():
+            raise ErrorParameterNotValid(
+                f"Invalid name. Numeric values are not permitted. "
+                f"Input: '{name_character}'"
+        )
+        return clean_name
+
+    @classmethod
+    def from_api(cls, data_response:List[dict]) -> List[People]:
+        return [cls.from_api_response(instance) for instance in data_response]
+
+    @classmethod
+    def get_character_names(cls, list_characters:List[People]) -> List[str]:
         all_characters_names=[]
-        characters = self.from_api()
-        for character in characters:
+        for character in list_characters:
             all_characters_names.append(character.name)
 
         return all_characters_names
 
-    def is_from_planet(self, name_planet:str) -> bool:
-        return self._homeworld == name_planet
+    def is_from_planet(cls, name_planet:str) -> bool:
+        name_planet = cls.validate_input(name_planet)
+        if cls._homeworld.lower() == name_planet.lower():
+            return True
+
     @property
     def name(self):
         return self._name
